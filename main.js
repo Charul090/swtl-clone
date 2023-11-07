3356b70b797148553f1c4ae43135d779760f4514
Charul <baricharul9@hotmail.com>
2023-11-07 20:25:04 +0530

    feat: add support for router plugins, fallback ui

--- main.js
+++ main.js
@@ -1,14 +1,27 @@
 import { Router } from './router';
+import { render } from './render';
 
 export const router = new Router(
-    [
-        {
-            path: '/',
-            render: () => (`<html><body><h1>Check 1234s</h1></body></html>`)
-        },
-        {
-            path: '/about',
-            render: () => (`<html><body><h1>About</h1></body></html>`)
-        }
-    ]
+    {
+        routes: [
+            {
+                path: '/',
+                render: () => (`<html><body><h1>Check 1234s</h1></body></html>`)
+            },
+            {
+                path: '/about',
+                render: () => (`<html><body><h1>About</h1></body></html>`)
+            },
+            {
+                path: '/local',
+                plugin: () => (`<html><body><h1>Render Local</h1></body></html>`)
+            },
+            {
+                path: '/stream',
+                plugin: () => (render(`<html><body><h1>Render Stream</h1></body></html>`))
+            }
+        ],
+        fallback: `<html><body><h1>404</h1></body></html>`,
+        plugin: () =>('<html><body><h1>Render global plugin</h1></body></html>')
+    }
 );
--- router.js
+++ router.js
@@ -3,17 +3,23 @@ import { render } from './render';
 export class Router {
     /**
      * 
-     * @param {*} routes Array<{ path: string, render: (params, query, request) => string }>
+     * @param {*} routes Array<{ path: string, render: (params, query, request) => string, plugin }>
+     * @param {*} fallback string
+     * @param {*} baseHref string
+     * @param {*} baseHref plugins
      */
-    constructor(routes) {
+    constructor({ routes, fallback, baseHref = '', plugin }) {
         this.routes = routes;
+        this.fallback = fallback;
+        this.plugin = plugin;
+        this.baseHref = baseHref
     }
 
     handleRequest (req) {
         const url = req.url;
         let pathInfo;
         const matchedRoute = this.routes.find(route => {
-            const urlPattern = new URLPattern({ pathname: route.path });
+            const urlPattern = new URLPattern({ pathname: this.baseHref ? this.baseHref + '/' : this.baseHref + route.path });
             const isMatch = urlPattern.test(url);
             if (isMatch) {
                 pathInfo = urlPattern.exec(url);
@@ -22,14 +28,30 @@ export class Router {
             return isMatch;
         });
 
-        const params = pathInfo.pathname.groups;
+        if (!matchedRoute) {
+            return render(this.fallback || '');
+        }
+
         const search = {};
         const searchParams = new URLSearchParams(pathInfo.search.groups[0]);
+        const params = pathInfo.pathname.groups;
+        
+        if (matchedRoute.plugin ?? this.plugin) {
+            const plugin = matchedRoute.plugin ?? this.plugin;
+            const pluginRes = plugin(params, search, req);
+            if (String(pluginRes) === '[object Response]') {
+                return pluginRes;
+            }
+            
+            return render(pluginRes);
+        }
+        
+        
         for (const [key, value] of searchParams.entries()) {
             search[key] = value;
         }
 
-        const htmlTemplate = matchedRoute.render(params, search, url);
+        const htmlTemplate = matchedRoute.render(params, search, req);
         return render(htmlTemplate);
     }
 }
